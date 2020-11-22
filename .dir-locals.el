6922067364f6169ee9a970312d7050444a334e15
Sibi Prabakaran <sibi@psibi.in>
2020-11-22 14:11:43 +0530

    Add scripts and publish site

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode
+  (org-twbs-link-up . "https://psibi.in/prometheus/")
+  (org-twbs-link-home . "https://psibi.in/")
+  (org-publish-project-alist . '("prometheus" 
+                                  :base-directory "~/github/prometheus/"
+                                  :recursive t
+                                  :base-extension "org"
+                                  :auto-sitemap t
+                                  :publishing-function org-twbs-publish-to-html
+                                  :publishing-directory "~/github/prometheus/docs/"))))
