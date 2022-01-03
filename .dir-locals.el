f9f995749d5e1887207617283b8f4d0998372278
Sibi Prabakaran <sibi@psibi.in>
2022-01-03 13:06:28 +0530

    Update prometheus site

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,10 +4,10 @@
 ((org-mode
   (org-twbs-link-up . "https://psibi.in/prometheus/")
   (org-twbs-link-home . "https://psibi.in/")
-  (org-publish-project-alist . '("prometheus" 
-                                  :base-directory "~/github/prometheus/"
-                                  :recursive t
-                                  :base-extension "org"
-                                  :auto-sitemap t
-                                  :publishing-function org-twbs-publish-to-html
-                                  :publishing-directory "~/github/prometheus/docs/"))))
+  (org-publish-project-alist . (("prometheus"
+                                    :base-directory "~/github/prometheus/"
+                                    :recursive t
+                                    :base-extension "org"
+                                    :auto-sitemap t
+                                    :publishing-function org-twbs-publish-to-html
+                                    :publishing-directory "~/github/prometheus/docs/" )))))
